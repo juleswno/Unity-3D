0a9871c9c220b6b1904a1ae15b3ecd925737cb8f
juleswno <reve0107@gmail.com>
2025-05-23 10:16:37 +0900

    design: UI
    update of UI components

---
+++ Assets/Scripts/UI/UICondition.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// consistent updates of UI Conditions
+public class UI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
